1c4d73a6f963ba5081811b2f41b7b51da7222a9e
Jonathan <jvazquez441@email.suagm.edu>
2020-03-08 20:14:23 -0400

    Add project files.

---
+++ FatPercentageCalculator/FatPercentageCalc.cpp
@@ -0,0 +1,39 @@
+/*
+Jonathan Vazquez Rivera
+jvazquez441@email.uagm.edu
+Computer Science Tutor
+
+Decision making laboratory
+Assigment for COSC-131 students
+
+Assigment:	Create an application that allows the user to enter the number of calories and fat grams in a food.
+			The application should display the percentage of the calories that come from fat. If the calories
+			from fat are less than 30% of the total calories of the food, it should also display a message
+			indicating the food is low in fat.
+
+			One gram of fat has 9 calories, so:
+			Calories from fat = fat grams * 9
+
+			The percentage of calories from fat can be calculated as:
+			Percentage of calories from fat = Calories from fat / total calories
+
+			Input validation: Make sure the number of calories are not less than 0. Also, the number of
+			calories from fat cannot be greater than the total number of calories. If that happens, display an
+			error message indicating that either the calories or fat grams were incorrectly entered.
+
+			Use the following test data to determine if the application is calculating properly:
+			Calories and Fat Percentage Fat
+			200 calories, 8 fat grams Percentage of calories from fat: 36%
+			150 calories 2 fat grams Percentage of calories from fat: 12%
+			(a low-fat food)
+			500 calories, 30 fat grams Percentage of calories from fat: 54%
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
